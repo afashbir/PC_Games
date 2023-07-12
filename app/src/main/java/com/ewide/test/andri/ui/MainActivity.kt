76398a78816840bb5885b4b4b7a08d0072132389
andrifashbir <af.7407@gmail.com>
2023-07-12 20:14:03 +0700

    Initial commit

---
+++ app/src/main/java/com/ewide/test/andri/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ewide.test.andri.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ewide.test.andri.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
